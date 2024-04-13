e762b228b725c30994806cbb61cb36476ff8169f
mistirnigusse <mistirnigusse00@gmail.com>
2024-04-13 11:28:24 +0300

    refactored

--- src/loader.py
+++ src/loader.py
@@ -6,39 +6,53 @@ import shutil
 import copy
 import csv
 from datetime import datetime
+from pick import pick
 from time import sleep
-
+import pandas as pd
 
 
 class NewsDataLoader:
 
     def __init__(self, path):
-       
+     
         self.path = path
-        self.news = self.get_news()
-        self.traffic = self.get_traffic()
-        self.domain_location = self.get_domain_location()
+        self.news = self.get_news_path()
+        self.traffic = self.get_traffic_path()
+        self.domain_location = self.get_domain_location_path()
     
 
-    def get_news(self):
-      
+    def get_news_path(self):
+       
         news = os.path.join(self.path, 'rating.csv')
         
         return news
     
-    def get_traffic(self):
-     
+    def get_traffic_path(self):
+       
         traffic = os.path.join(self.path, 'traffic.csv')
-
-        return traffic  
+        
+        return traffic
       
-    def get_domain_location(self):
- 
+    def get_domain_location_path(self):
+      
         domain_location = os.path.join(self.path, 'domains_location.csv')
 
         return domain_location
 
 
+    def get_news_data(self):
+       
+        return pd.read_csv(self.get_news_path())
+    
+    def get_traffic_data(self):
+      
+        
+        return pd.read_csv(self.get_traffic_path())
+      
+    def get_domain_location_data(self):
+       
+
+        return pd.read_csv(self.get_domain_location_path())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Export News history')
